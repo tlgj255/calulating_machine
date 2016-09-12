6ad405cd92ee7ab7515adce84ffb8c48f988d7e5
tlgj255 <tlgj255@naver.com>
2016-09-12 20:29:03 +0900

    계산기

---
+++ app/src/main/java/com/example/tlqkf/calculating_machine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tlqkf.calculating_machine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
