4040c1de142d30a6d5f20f0c068ac8c4d30e1012
tlgj255 <tlgj255@naver.com>
2016-09-18 02:11:46 +0900

    계산기(사칙연산만(일부분))

--- app/src/main/java/com/example/tlqkf/calculating_machine/MainActivity.java
+++ app/src/main/java/com/example/tlqkf/calculating_machine/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.tlqkf.calculating_machine;
 
 import android.content.pm.ActivityInfo;
-import android.icu.text.DecimalFormat;
-import android.icu.text.DecimalFormatSymbols;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    int num1 = 0, maNum = 0;
-    Button n1, n2, n3, n4, n5, n6, n7, n8, n9, n0, result, reset, division, plus, multiply, subtrack, bracket, back, point, switch_;
-    TextView save_result, show_result, show_n;
+    int num[] = new int[4], maNum[] = new int[4], a = 0, num2 = 0, b = 0;
+    String s;
+    Button n1, n2, n3, n4, n5, n6, n7, n8, n9, n0, result, reset, division, plus, multiply, subtrack, bracket, back, n00, switch_;
+    TextView show_result, show_n;
 
 
     @Override
@@ -42,9 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         subtrack = (Button) findViewById(R.id.subtrack);
         bracket = (Button) findViewById(R.id.bracket);
         back = (Button) findViewById(R.id.back);
-        point = (Button) findViewById(R.id.point);
+        n00 = (Button) findViewById(R.id.n00);
         switch_ = (Button) findViewById(R.id.switch_);
-        save_result = (TextView) findViewById(R.id.save_result);
         show_result = (TextView) findViewById(R.id.show_result);
         show_n = (TextView) findViewById(R.id.show_n);
         n1.setOnClickListener(this);
@@ -65,116 +61,202 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         subtrack.setOnClickListener(this);
         bracket.setOnClickListener(this);
         back.setOnClickListener(this);
-        point.setOnClickListener(this);
+        n00.setOnClickListener(this);
         switch_.setOnClickListener(this);
         show_n.setOnClickListener(this);
-
-
     }
 
     @Override
     public void onClick(View v) {
-        show_n.setText("" + num1);
-        Log.i("num1값", "" + num1);
-        Log.i("maNum값", "" + maNum);
-        Toast over = Toast.makeText(MainActivity.this,"입력 가능한 9자를 초과했습니다.",Toast.LENGTH_SHORT);
+        Toast over = Toast.makeText(MainActivity.this, "입력 가능한 9자를 초과했습니다.", Toast.LENGTH_SHORT);
+        switch (v.getId()) {
+            case R.id.plus:
+                if (num[a] > 0 && b == 0) {
+                    b = 1;
+                    a++;
+                    s = "+";
+                }
+                break;
+            case R.id.division:
+                if (num[a] > 0 && b == 0) {
+                    b = 2;
+                    a++;
+                    s = "%";
+                }
+                break;
+            case R.id.subtrack:
+                if (num[a] > 0 && b == 0) {
+                    b = 3;
+                    a++;
+                    s = "-";
+                }
+                break;
+            case R.id.multiply:
+                if (num[a] > 0 && b == 0) {
+                    b = 4;
+                    a++;
+                    s = "X";
+                }
+        }
         switch (v.getId()) {
             case R.id.n1:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1++;
-
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    num[a] = num[a] * 10;
+                    num[a]++;
+                    b = 0;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n2:
-                maNum++;
-                if (maNum < 10) {
-                    num1 = num1 * 10;
-                    num1 = num1 + 2;
-                }else{
+                if (maNum[a] < 10) {
+                    b = 0;
+                    maNum[a]++;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 2;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n3:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 3;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 3;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n4:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 4;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 4;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n5:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 5;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 5;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n6:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 6;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 6;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n7:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 7;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 7;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n8:
-                maNum++;
-                if (maNum < 10) {
-                    num1 = num1 * 10;
-                    num1 = num1 + 8;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 8;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n9:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                    num1 = num1 + 9;
-                }else{
+                if (maNum[a] < 10) {
+                    maNum[a]++;
+                    b = 0;
+                    num[a] = num[a] * 10;
+                    num[a] = num[a] + 9;
+                } else {
                     over.show();
                 }
                 break;
             case R.id.n0:
-                if (maNum < 10) {
-                    maNum++;
-                    num1 = num1 * 10;
-                }else{
-                    over.show();
+                if (num[a] > 0) {
+                    if (maNum[a] < 10) {
+                        b = 0;
+                        maNum[a]++;
+                        num[a] = num[a] * 10;
+                    } else {
+                        over.show();
+                    }
+                }
+                break;
+            case R.id.n00:
+                if (num[a] > 0) {
+                    if (maNum[a] < 7) {
+                        b = 0;
+                        maNum[a] = maNum[a] + 2;
+                        num[a] = num[a] * 100;
+                    } else if (maNum[a] < 9) {
+                        maNum[a]++;
+                        num[a] = num[a] * 10;
+                    } else
+                        over.show();
                 }
                 break;
             case R.id.back:
-                if (maNum < 10) {
-                    maNum--;
+                if (maNum[a] > 0) {
+                    maNum[a]--;
+                    num[a] = num[a] * 1 / 10;
                 }
                 break;
+            case R.id.reset:
         }
-
+        if (s == null)
+            show_n.setText("" + num[0]);
+        else if (s != null)
+            show_n.setText("" + num[0] + s);
+        if (s != null && num[1] > 0)
+            show_n.setText("" + num[0] + s + num[1]);
+        if (a == 2) {
+            a = 1;
+            if (b == 1) {
+                num[0] = num[0]+num[1];
+                show_n.setText("" + num[0]+s);
+                b = num[0];
+                show_result.setText(""+b);
+            }
+            if (b == 2) {
+                num[0] = num[0]/num[1];
+                show_n.setText("" + num[0]+s);
+                b = num[0];
+                show_result.setText(""+b);
+            }
+            if (b == 3) {
+                num[0] = num[0]-num[1];
+                show_n.setText("" + num[0]+s);
+                b = num[0];
+                show_result.setText(""+b);
+            }
+            if (b == 4) {
+                num[0] = num[0]*num[1];
+                show_n.setText("" + num[0]+s);
+                b = num[0];
+                show_result.setText(""+b);
+            }
+            num[1] = 0;
+            maNum[1] = 0;
+        }
+        Log.i("num[" + a + "]값", "" + num[a]);
+        Log.i("maNum[" + a + "]값", "" + maNum[a]);
     }
 }
